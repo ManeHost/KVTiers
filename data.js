47e05f9a53b74c5b29c0fbd40b930bbd6cbf2c56
ManeHost <hasanovrasul792@gmail.com>
2026-08-09 20:34:14 +0500

    Fix formatting issue in data.js

--- data.js
+++ data.js
@@ -104,7 +104,7 @@ const PLAYERS = [
     name: "0993472",
     region: "UA",
     tiers: {
-      beast: "LT3", dpot: "HT4", npot: "HT4", dsmp: "HT4", nsmp: "HT4,
+      beast: "LT3", dpot: "HT4", npot: "HT4", dsmp: "HT4", nsmp: "HT4",
       uhc: "LT4", axe: "LT4", crystal: "LT4", cart: "none", mace: "LT3",
       op: "HT4", creeper: "none", spearmace: "none", spearelytra: "none",
       rodmace: "none", trident: "none", shieldsuhc: "none", elytramace: "none",
